9f19d6c67f353478dd44bc117a3ff8b69697567e
Anton Tsarenko <itantontsarenko@gmail.com>
2026-06-27 10:42:27 +0200

    hotfix: update POSTGRES in UserRepositoryTest.java

--- src/test/java/com/anton/tsarenko/payment/core/api/users/repository/UserRepositoryTest.java
+++ src/test/java/com/anton/tsarenko/payment/core/api/users/repository/UserRepositoryTest.java
@@ -25,7 +25,7 @@ class UserRepositoryTest {
     @Container
     @ServiceConnection
     @SuppressWarnings("unused")
-    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:17-alpine");
+    private static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer("postgres:17-alpine");
 
     @Autowired
     private UserRepository userRepository;
